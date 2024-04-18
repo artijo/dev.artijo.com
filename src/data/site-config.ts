e27ebc91c859791f29c0ae7c242bceae63980913
tijo <81065469+artijo@users.noreply.github.com>
2024-04-18 20:52:21 +0700

    Config & Fonts

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -38,12 +38,12 @@ export type SiteConfig = {
 };
 
 const siteConfig: SiteConfig = {
-    title: 'Dante',
-    subtitle: 'Minimal Astro.js theme',
-    description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
+    title: 'ARTIJO • DEV',
+    subtitle: 'Web Developer & Tech Enthusiast',
+    description: `I'm a web developer and a tech enthusiast. I write about web development, programming, and tech.`,
     image: {
-        src: '/dante-preview.jpg',
-        alt: 'Dante - Astro.js and Tailwind CSS theme'
+        src: '/preview.jpg',
+        alt: 'artijo.dev preview image'
     },
     headerNavLinks: [
         {
@@ -69,35 +69,27 @@ const siteConfig: SiteConfig = {
             href: '/contact'
         },
         {
-            text: 'Terms',
-            href: '/terms'
+            text: 'Privacy Policy',
+            href: 'https://privacy.artijo.com/'
         },
-        {
-            text: 'Download theme',
-            href: 'https://github.com/JustGoodUI/dante-astro-theme'
-        }
     ],
     socialLinks: [
         {
-            text: 'Dribbble',
-            href: 'https://dribbble.com/'
-        },
-        {
-            text: 'Instagram',
-            href: 'https://instagram.com/'
+            text: 'GitHub',
+            href: 'https://github.com/artijo'
         },
         {
             text: 'X/Twitter',
-            href: 'https://twitter.com/'
+            href: 'https://twitter.com/artijo_'
         }
     ],
     hero: {
-        title: 'Hi There & Welcome to My Corner of the Web!',
-        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
-        image: {
-            src: '/hero.jpeg',
-            alt: 'A person sitting at a desk in front of a computer'
-        },
+        title: "Hi, I'm Artijo! 👋",
+        text: "I'm a web developer and a tech enthusiast. I write about web development, programming, and tech.",
+        // image: {
+        //     src: '/hero.jpeg',
+        //     alt: 'A person sitting at a desk in front of a computer'
+        // },
         actions: [
             {
                 text: 'Get in Touch',
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,8 +5,8 @@ module.exports = {
     darkMode: 'class',
     theme: {
         fontFamily: {
-            sans: ['Inter', ...defaultTheme.fontFamily.sans],
-            serif: ['Newsreader', ...defaultTheme.fontFamily.serif]
+            sans: ['chulabhornlikit', ...defaultTheme.fontFamily.sans],
+            serif: ['lineseed', ...defaultTheme.fontFamily.serif]
         },
         extend: {
             textColor: {
